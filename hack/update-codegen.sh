ed18369b14befa58b64952821a178974fa02e28e
wm775825 <wm775825@mail.ustc.edu.cn>
2021-12-31 16:23:53 +0800

    Uploading for code-gen on Linux

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -18,15 +18,13 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-SCRIPT_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
-CODEGEN_PKG=${CODEGEN_PKG:-$(cd "${SCRIPT_ROOT}"; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo ../code-generator)}
-
 # generate the code with:
 # --output-base    because this script should also be able to run inside the vendor dir of
 #                  k8s.io/kubernetes. The output-base is needed for the generators to output into the vendor dir
 #                  instead of the $GOPATH directly. For normal projects this can be dropped.
-"${CODEGEN_PKG}"/generate-groups.sh "deepcopy,client,informer,lister" \
-  github.com/wm775825/sync-controller/pkg/generated github.com/wm775825/sync-controller/pkg/apis \
+$(pwd)/vendor/k8s.io/code-generator/generate-groups.sh "deepcopy,client,informer,lister" \
+  ./pkg/generated ./pkg/apis \
   simage:v1alpha1 \
-  --output-base "$(dirname "${BASH_SOURCE[0]}")/../../../../.." \
-  --go-header-file "${SCRIPT_ROOT}"/hack/boilerplate.go.txt
+  --output-base "$(pwd)/" \
+  --go-header-file $(pwd)/hack/boilerplate.go.txt \
+  -v 10
